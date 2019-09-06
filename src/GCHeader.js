f8f147256cb71e213442f32dcfcd4c046c9be099
Jordan Morrison <32398038+Jordan-Morrison@users.noreply.github.com>
2019-09-06 09:57:10 -0400

    Initial Commit

---
+++ src/GCHeader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function GCHeader() {
+    return (
+        <div>
+            <img src="./FIPS/Canada_Red_FIP.png"/>
+        </div>
+    );
+}
+
+export default GCHeader;
---
+++ src/index.js
@@ -0,0 +1,1 @@
+export {GCHeader} from "./GCHeader";
