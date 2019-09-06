6ca930fa887109a41577ba446301f5120aa9ecbd
Jordan Morrison <32398038+Jordan-Morrison@users.noreply.github.com>
2019-09-06 09:59:51 -0400

    Testing

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-export {GCHeader} from "./GCHeader";
+// export {GCHeader} from "./GCHeader";
+
+import React from 'react';
+
+function GCHeader() {
+    return (
+        // <div>
+        //     <img src="./FIPS/Canada_Red_FIP.png"/>
+        // </div>
+        <h1>Hello World</h1>
+    );
+}
+
+export default GCHeader;
